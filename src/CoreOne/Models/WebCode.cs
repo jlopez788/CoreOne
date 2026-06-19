35db5eb4491d7d6e277e0eda6a3b90eb9cae75fb
Juan <Juan.Lopez1@drivetime.com>
2026-06-19 12:39:05 -0400

    Adding webcode

---
+++ src/CoreOne/Models/WebCode.cs
@@ -0,0 +1,21 @@
+﻿namespace CoreOne.Models;
+
+public class WebCode
+{
+    public static implicit operator bool(WebCode? webCode) => webCode?.IsSuccessStatusCode == true;
+
+    public int Code { get; init; }
+    public bool IsSuccessStatusCode { get; }
+
+    public WebCode(int code)
+    {
+        Code = code;
+        IsSuccessStatusCode = WebCodes.IsSuccessStatusCode(code);
+    }
+
+    public WebCode(int? code)
+    {
+        Code = code.GetValueOrDefault();
+        IsSuccessStatusCode = WebCodes.IsSuccessStatusCode(Code);
+    }
+}
